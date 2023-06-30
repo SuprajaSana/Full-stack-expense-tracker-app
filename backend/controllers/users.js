38c65c0f80f1b1b1f673b966531afce2b3eda90b
SuprajaSana <sanasupraja2727@gmail.com>
2023-06-30 15:19:22 +0530

    password changes with bcrypt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const UserDetails = require("../models/users");
+const bcrypt = require("bcrypt");
 
 function isStringValid(s) {
   if (s == undefined || s.length === 0) {
@@ -18,22 +19,31 @@ exports.signUpUserDetails = async (req, res, next) => {
     isStringValid(email) ||
     isStringValid(password)
   ) {
-    return res.status(400).json({ success:false,message: "Required all fields" });
+    return res
+      .status(400)
+      .json({ success: false, message: "Required all fields" });
   }
 
-  await UserDetails.create({
-    userName: userName,
-    email: email,
-    password: password,
-  })
-    .then((details) => {
+  const saltrounds = 10;
+  bcrypt.hash(password, saltrounds, async (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
+    await UserDetails.create({
+      userName: userName,
+      email: email,
+      password: hash,
+    }).then((details) => {
       res.status(201).json({
-        success:true,
+        success: true,
         message: "Successfully created new user",
         userDetail: details,
       });
     })
-    .catch((err) => res.status(500).json({ success:false,message:err }));
+      .catch(err => {
+       res.status(500).json({ success: false, message: err });
+    })
+  });
 };
 
 exports.loginUserDetails = async (req, res, next) => {
@@ -41,21 +51,28 @@ exports.loginUserDetails = async (req, res, next) => {
   const password = req.body.password;
 
   if (isStringValid(email) || isStringValid(password)) {
-    return res.status(400).json({ success:false,message: "Required all fields" });
+    return res
+      .status(400)
+      .json({ success: false, message: "Required all fields" });
   }
 
   await UserDetails.findAll({ where: { email: email } })
     .then((details) => {
       if (details.length > 0) {
-        if (details[0].password === password) {
-          res
-            .status(200)
-            .json({ success: true, message: "User login successful" });
-        } else {
-          res
-            .status(401)
-            .json({ success: false, message: "User not authorized" });
-        }
+        bcrypt.compare(password, details[0].password, (err, result) => {
+          if (err) {
+            throw new Error("Something went wrong");
+          }
+          if (result === true) {
+            res
+              .status(200)
+              .json({ success: true, message: "User login successful" });
+          } else {
+            res
+              .status(401)
+              .json({ success: false, message: "Password does not match" });
+          }
+        })
       } else {
         res.status(404).json({ success: false, message: "User not found" });
       }
