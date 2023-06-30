d6cd5f9f86c6ca0b6860274d2b300b7e27487ab6
SuprajaSana <sanasupraja2727@gmail.com>
2023-06-30 12:44:27 +0530

    changes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,6 @@
 const UserDetails = require("../models/users");
 
-function isStringValid(s){
+function isStringValid(s) {
   if (s == undefined || s.length === 0) {
     return true;
   } else {
@@ -13,8 +13,12 @@ exports.signUpUserDetails = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (isStringValid(userName) || isStringValid(email) || isStringValid(password)) {
-    return res.status(400).json({err:"Required all fields"})
+  if (
+    isStringValid(userName) ||
+    isStringValid(email) ||
+    isStringValid(password)
+  ) {
+    return res.status(400).json({ success:false,message: "Required all fields" });
   }
 
   await UserDetails.create({
@@ -23,25 +27,40 @@ exports.signUpUserDetails = async (req, res, next) => {
     password: password,
   })
     .then((details) => {
-      res.status(201).json({ userDetail: details,message:"Successfully created new user" });
+      res.status(201).json({
+        success:true,
+        message: "Successfully created new user",
+        userDetail: details,
+      });
     })
-    .catch((err) => res.status(500).json({ error: err }));
+    .catch((err) => res.status(500).json({ success:false,message:err }));
 };
 
 exports.loginUserDetails = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (isStringValid(email) || isStringValid(password)) {
+    return res.status(400).json({ success:false,message: "Required all fields" });
+  }
+
   await UserDetails.findAll({ where: { email: email } })
     .then((details) => {
-      if (details[0].password === password) {
-        res.status(200).json({message:"User login successful"})
+      if (details.length > 0) {
+        if (details[0].password === password) {
+          res
+            .status(200)
+            .json({ success: true, message: "User login successful" });
+        } else {
+          res
+            .status(401)
+            .json({ success: false, message: "User not authorized" });
+        }
       } else {
-        res.status(401).json({message:"User not authorized"})
+        res.status(404).json({ success: false, message: "User not found" });
       }
     })
-    .catch(err=>{
-    res.status(404).json({error:err,message:"User not found"})
-  })
-
-}
+    .catch((err) => {
+      res.status(500).json({ success: false, message: err });
+    });
+};
